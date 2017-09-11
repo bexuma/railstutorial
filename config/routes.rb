7bf08ac1fe29e740c18d211b16647c9ab1397004
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2017-09-11 19:52:17 +0900

    Added sessions and created and integration test for logging with invalid credentials asserting the flash appearing once and disappering

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  
+
+  # Sessions
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # Static Pages
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
 
+  # Users
   resources :users
   get  '/signup',  to: 'users#new'
-  post '/signup', to: 'users#create'
+  post '/signup',  to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
